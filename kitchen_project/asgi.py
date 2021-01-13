3599d407924f7a08124cf381bed263b953bbf0f4
prettyirrelevant <ienioladewumi@gmail.com>
2021-01-13 10:05:20 +0100

    prod stuff

--- kitchen_project/asgi.py
+++ kitchen_project/asgi.py
@@ -1,14 +1,15 @@
 import os
 
 import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kitchen_project.settings.development")
+django.setup()
+
 from channels.auth import AuthMiddlewareStack
 from channels.http import AsgiHandler
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 import orders.routing
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kitchen_project.settings.development")
-django.setup()
 
 application = ProtocolTypeRouter(
     {
